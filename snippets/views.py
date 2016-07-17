ad54a711dddc2416077dadd50a752e97023ce1fb
ameenkhaan07 <ameenkhan07@gmail.com>
2016-07-17 20:15:17 +0530

    Permissions

--- snippets/views.py
+++ snippets/views.py
@@ -3,6 +3,7 @@ from rest_framework import generics
 from rest_framework import permissions
 from snippets.models import Snippet
 from django.contrib.auth.models import User
+from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer
 from snippets.serializers import UserSerializer
 
@@ -24,8 +25,7 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
-    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
-
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,IsOwnerOrReadOnly)
 
 class UserList(generics.ListAPIView):
 
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
 	url(r'^', include('snippets.urls')),
 	url(r'^users/$', views.UserList.as_view()),
-	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view())
+	url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
+	url(r'^api-auth/', include('rest_framework.urls',
+                               namespace='rest_framework')),
 ]
